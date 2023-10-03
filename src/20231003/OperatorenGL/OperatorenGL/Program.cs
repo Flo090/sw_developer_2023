60040db542c88cbe5056ba29efc60a73d5138351
Florian@WIFI <User@DEV-VM>
2023-10-03 21:23:39 +0200

    Start with arithmetic operators

---
+++ src/20231003/OperatorenGL/OperatorenGL/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorenGL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arithmetische Operatoren
+            //+ - / * %
+
+            double flaecheRechteck = 0.0;
+            double seiteA = 0.0;
+            double seiteB = 0.0;
+            flaecheRechteck = (seiteA + seiteB) * 2;
+        }
+    }
+}
