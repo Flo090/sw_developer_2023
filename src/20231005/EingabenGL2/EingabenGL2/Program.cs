524e893663f111fec0ed4d6436c96a0aac1e209a
Florian@WIFI <User@DEV-VM>
2023-10-05 20:01:54 +0200

    Grundlagen Console.ReadLine()

---
+++ src/20231005/EingabenGL2/EingabenGL2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EingabenGL2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = string.Empty;
+            string eingabe = string.Empty;
+            int alter = 0;
+
+            Console.Write("Bitte Name eingeben: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine($"Hallo {name}, wie geht's?");
+
+            Console.Write("Bitte Alter eingeben: ");
+            eingabe = Console.ReadLine();
+
+            //eingabe = "40"
+            alter = int.Parse(eingabe);
+
+            Console.WriteLine($"{name}, Sie sind {alter} Jahre jung.");
+            Console.WriteLine($"Sie wurden im Jahr {2023 - alter} geboren.");
+        }
+    }
+}
