5a271b75d08c1e073b343300d1c24d11a2a253de
Florian <User@DEVELOPERPC-03>
2023-10-17 20:06:39 +0200

    Basic Umsetzung Teilnehmer Verwaltung V2

---
+++ src/20231017/TeilnehmerVerwaltung_v2/TeilnehmerVerwaltung_v2/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TeilnehmerVerwaltung_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implementieren Sie eine Applikation mit der beliebig viele Teilnehmerdaten erfasst und dargestellt
+            // werden können. Folgende Anforderung sollen dabei erfüllt werden:
+
+            // - max.Anzahl der Teilnehmer soll zu Beginn vom User abgefragt werden
+            // - Einlesen folgender Daten:
+            //     - Name, Vorname
+            //     - Geburtsdatum
+            //     - PLZ, Ort
+            // - Fehlertolerante Eingaben
+            // - verwenden sie Methoden wo sinnvoll
+            // - verwenden sie Farben
+            // - Teilnehmerdaten sollen nach der Eingabe tabellarisch ausgegeben werden
+
+            string name = string.Empty;
+            string vorname = string.Empty;
+            DateTime geburtsdatum = DateTime.MinValue;
+            int plz = 0;
+            string ort = string.Empty;
+            string headerText = "Teilnehmer Verwaltung v2.0   (c) 2023 Wifi-Soft";
+
+            // Header
+            CreateHeader(headerText, ConsoleColor.Yellow, true);
+
+            // Daten einlesen
+            Console.WriteLine("Bitte geben Sie die Teilnehmerdaten ein: ");
+
+            vorname = ReadString("\tVorname: ");
+            name = ReadString("\tNachname: ");
+            geburtsdatum = ReadDateTime("\tGeburtsdatum: ");    // Eingabe Geburtsdatum => Methode weil komplex
+            plz = ReadInt("\tPLZ: ");                           // Eingabe PLZ => Methode weil komplex
+            ort = ReadString("\tWohnort: ");
+
+            // Ausgabe der Daten
+            DisplayStudentInfo(vorname, name, geburtsdatum, plz, ort);
+        }
+
+        private static void DisplayStudentInfo(string vorname, string name, DateTime geburtsdatum, int plz, string ort)
+        {
+            Console.WriteLine("\nDie Teilnehmerdaten: \n");
+            Console.WriteLine($"\t{vorname}, {name}, {geburtsdatum.ToShortDateString()}, {plz}, {ort}");
+        }
+
+        private static string ReadString(string inputPrompt)
+        {
+            string input = string.Empty;
+
+            do
+            {
+                Console.Write(inputPrompt);
+                input = Console.ReadLine();
+            }
+            while (string.IsNullOrEmpty(input));
+
+            return input;
+        }
+
+        private static int ReadInt(string inputPrompt)
+        {
+            string input = string.Empty;
+            int inputValue = 0;
+            bool inputIsValid = false;
+
+            do
+            {
+                Console.Write(inputPrompt);
+                input = Console.ReadLine();
+
+                try
+                {
+                    inputValue = int.Parse(input);
+                    inputIsValid = true;
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\aERROR: Ungültige Eingabe.");
+                    inputValue = 0;
+                    Console.ResetColor();
+                    inputIsValid = false;
+                }
+
+            }
+            while (!inputIsValid);
+
+            return inputValue;
+        }
+
+        private static DateTime ReadDateTime(string inputPrompt)
+        {
+            string input = string.Empty;
+            DateTime inputDateTime = DateTime.MinValue;
+            bool inputIsValid = false;
+
+            do
+            {
+                Console.Write(inputPrompt);
+                input = Console.ReadLine();
+
+                try
+                {
+                    inputDateTime = DateTime.ParseExact(input, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+                    inputIsValid = true;
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\aERROR: Ungültige Datumseingabe.");
+                    inputDateTime = DateTime.MinValue;
+                    Console.ResetColor();
+                    inputIsValid = false;
+                }
+
+            }
+            while (!inputIsValid);
+
+            return inputDateTime;
+        }
+
+        private static void CreateHeader(string headerText, ConsoleColor headerTextColor, bool clearScreen)
+        {
+            int xTitelPos = 0;
+
+            // Wenn true wird Bildschirm gelöscht
+            if (clearScreen)
+            {
+                Console.Clear();
+            }
+
+            //Darstellung Programm Header
+            Console.WriteLine(new string('*', Console.WindowWidth - 1));
+            Console.WriteLine(new string('*', Console.WindowWidth - 1));
+            Console.WriteLine(new string('*', Console.WindowWidth - 1));
+
+            //cursor f. Titel Ausgabe positionieren
+            xTitelPos = (Console.WindowWidth - (headerText.Length + 2)) / 2;
+            Console.SetCursorPosition(xTitelPos, 1);
+
+            ConsoleColor oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = headerTextColor;
+            Console.Write(" " + headerText + " ");
+            Console.ForegroundColor = oldColor;
+
+            //alte cursor Position wiederherstellen
+            Console.SetCursorPosition(0, 4);
+            //Consolen Fensterbezeichnung setzen
+            Console.Title = headerText;
+        }
+    }
+}
