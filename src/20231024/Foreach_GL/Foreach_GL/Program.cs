0dfdd9a86b15a7ade64c2f5371dea1778b042362
Florian <User@DEVELOPERPC-03>
2023-10-24 18:26:19 +0200

    Grundlagen Foreach Iteration

---
+++ src/20231024/Foreach_GL/Foreach_GL/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreach_GL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Deklaration, Dimensionierung, Initialisierung
+            string[] names = new string[]
+            {
+                "Gandalf", "Sauron", "Eomer", "Alf", "Alice"
+            };
+
+            for(int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+                //names[i] = string.Empty;
+            }
+
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+                //name = string.Empty;
+            }
+        }
+    }
+}
