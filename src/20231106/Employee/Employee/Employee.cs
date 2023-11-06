094bb93eeb08f4dceb85b1949926eb8d77240d35
Florian <User@DEVELOPERPC-03>
2023-11-06 20:02:38 +0100

    Grundlagen Klassen & Konstruktoren

---
+++ src/20231106/Employee/Employee/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    internal class Employee
+    {
+        public string Name;
+        public Guid Id;
+
+        public Employee(string name)
+        {
+            Name = name;
+            Id = Guid.NewGuid();
+        }
+
+        public Employee(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Employee");
+            Console.WriteLine($"Name:\t{Name}");
+            Console.WriteLine($"Id:\t{Id}");
+        }
+    }
+}
