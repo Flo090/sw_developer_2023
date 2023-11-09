b8bc806e64183f6503d7a04ca34d5a796f149e7c
Florian <User@DEVELOPERPC-03>
2023-11-09 20:17:28 +0100

    Polymorphie virtual-override

---
+++ src/20231109/Polymorphie/Polymorphie/ShapeTypes/Vieleck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphie.ShapeTypes
+{
+    internal class Vieleck : Shape
+    {
+        public Vieleck(string description, int cornerCount, ConsoleColor color)
+            : base(description, cornerCount, color)
+        {
+
+        }
+    }
+}
