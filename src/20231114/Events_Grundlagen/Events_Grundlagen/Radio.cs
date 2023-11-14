8a66624f79800b28d44b82da30ec840f1950e89e
Florian <User@DEVELOPERPC-03>
2023-11-14 21:40:55 +0100

    Events Grundlagen

---
+++ src/20231114/Events_Grundlagen/Events_Grundlagen/Radio.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_Grundlagen
+{
+    internal delegate void PowerStateChangedHandler(object sender, PowerStateChangedEventArgs e);
+
+    internal class Radio
+    {
+        private int _frequence;
+        private DevicePower _powerState;
+
+        public Radio()
+        {
+            _frequence = 90;
+            _powerState = DevicePower.Off;
+        }
+        public Radio(int frequence, DevicePower powerState)
+        {
+            _frequence = frequence;
+            _powerState = powerState;
+        }
+
+        public event PowerStateChangedHandler PowerStateChanged;
+
+        public int Frequence
+        {
+            get { return _frequence; }
+            set
+            {
+                if (value >= 87.6 && value <= 107.9)
+                {
+                    _frequence = value;
+                }
+            }
+        }
+        public DevicePower PowerState
+        {
+            get { return _powerState; }
+        }
+
+        public void ShowState()
+        {
+            Console.WriteLine("Radio:");
+            Console.WriteLine($"Power State: {_powerState}");
+            Console.WriteLine($"Frequence: {_frequence} MHz");
+            Console.WriteLine();
+        }
+        public void Play()
+        {
+            if (_powerState == DevicePower.On)
+            {
+                Console.WriteLine($"Play at Frequence {_frequence} MHz");
+            }
+        }
+        public void ChangePowerState(DevicePower newPowerState)
+        {
+            if (_powerState == newPowerState)
+            {
+                return;
+            }
+
+            switch (newPowerState)
+            {
+                case DevicePower.Off:
+                case DevicePower.Standby:
+                case DevicePower.On:
+                    var oldPowerState = _powerState;
+                    _powerState = newPowerState;
+
+                    // Erreignis abfeuern
+                    OnPowerStateChanged(oldPowerState, newPowerState);
+                    
+                    break;
+
+                case DevicePower.Unknown:
+                case DevicePower.Defective:
+                    // Status kann nicht von ext. gesetzt werden
+                    break;
+
+                default:
+                    throw new ArgumentException("Ungültiger DeviceState entdeckt !!!");
+            }
+        }
+
+        protected void OnPowerStateChanged(DevicePower oldPowerState, DevicePower newPowerState)
+        {
+            var eventArgs = new PowerStateChangedEventArgs(oldPowerState, newPowerState);
+            PowerStateChanged?.Invoke(this, eventArgs);
+        }
+    }
+}
