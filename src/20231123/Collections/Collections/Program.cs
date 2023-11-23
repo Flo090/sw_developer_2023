0317ed6bffeee9c7758fbb72682ab823c4a9ff25
Florian <User@DEVELOPERPC-03>
2023-11-23 19:11:25 +0100

    Collections (List, IEnumerable)

---
+++ src/20231123/Collections/Collections/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //alt = Array
+            //var nameList = new string[]
+            //{
+            //    ""
+            //};
+
+            var nameList = new List<string>();
+            var dateList = new List<DateTime>();
+
+            nameList.Add("Gandalf");
+            nameList.Add("Hans");
+            nameList.Add("Max");
+
+            nameList.Remove("Hans");
+
+            DisplayItems(nameList);
+        }
+
+        private static void DisplayItems(IEnumerable<string> nameList)
+        {
+            foreach (var item in nameList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
