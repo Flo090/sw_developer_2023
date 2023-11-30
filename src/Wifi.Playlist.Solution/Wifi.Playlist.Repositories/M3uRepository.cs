f41a70c27c57edf59cc6fc827e30f96ac0f626f9
Florian <User@DEVELOPERPC-03>
2023-11-30 21:35:05 +0100

    Define IPlaylistRepository

---
+++ src/Wifi.Playlist.Solution/Wifi.Playlist.Repositories/M3uRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wifi.Playlist.CoreTypes;
+
+namespace Wifi.Playlist.Repositories
+{
+    internal class M3uRepository : IPlaylistRepository
+    {
+        public string Description => throw new NotImplementedException();
+
+        public string Extension => throw new NotImplementedException();
+
+        public IPlaylist Load(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(IPlaylist playList, string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
