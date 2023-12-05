8ee755e0c9764cd2faa27bf8efedeab049df2786
Florian <User@DEVELOPERPC-03>
2023-12-05 19:59:17 +0100

    Create filedialog filter & setup

--- src/Wifi.Playlist.Solution/Wifi.Playlist.Repositories/M3uRepository.cs
+++ src/Wifi.Playlist.Solution/Wifi.Playlist.Repositories/M3uRepository.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using PlaylistsNET.Content;
+using PlaylistsNET.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,20 +10,40 @@ using Wifi.Playlist.CoreTypes;
 
 namespace Wifi.Playlist.Repositories
 {
-    internal class M3uRepository : IPlaylistRepository
+    public class M3uRepository : IPlaylistRepository
     {
-        public string Description => throw new NotImplementedException();
+        public string Description => "M3U file";
 
-        public string Extension => throw new NotImplementedException();
+        public string Extension => ".m3u";
 
         public IPlaylist Load(string filePath)
         {
             throw new NotImplementedException();
         }
 
-        public void Save(IPlaylist playList, string filePath)
+        public void Save(IPlaylist playlist, string filePath)
         {
-            throw new NotImplementedException();
+            M3uPlaylist m3uPlaylist = new M3uPlaylist();
+            m3uPlaylist.IsExtended = true;
+
+            foreach (IPlaylistItem item in playlist.Items)
+            {
+                m3uPlaylist.PlaylistEntries.Add(new M3uPlaylistEntry()
+                {
+                    Title = item.Title,
+                    AlbumArtist = item.Author,
+                    Duration = item.Duration,
+                    Path = item.FilePath
+                });
+            }
+            M3uContent content = new M3uContent();
+            string text = content.ToText(m3uPlaylist);
+
+            using (StreamWriter writer = new StreamWriter(filePath, false))
+            {
+                writer.WriteLine(text);
+                writer.Close();
+            }
         }
     }
 }
